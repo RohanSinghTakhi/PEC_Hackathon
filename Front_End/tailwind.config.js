273ec7c1207e206bee11555dd89eb95d92ccb05d
RohanSinghTakhi <co23545@ccet.ac.in>
2024-10-13 21:48:35 +0530

    basic setup done

---
+++ Front_End/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This includes all your React components
+    "./public/index.html", // If you're using any HTML files for your templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
